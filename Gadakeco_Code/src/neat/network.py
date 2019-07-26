434fd18823132f68a4fbd68c131d6862f053c36b
uqveo <uqveo@student.kit.edu>
2019-07-26 22:13:14 +0200

    basic idea to realize the neat

--- Gadakeco_Code/src/neat/network.py
+++ Gadakeco_Code/src/neat/network.py
@@ -1,9 +1,17 @@
 
+import numpy as np
+import itertools
+import random
 
 class Network:
     def __init__(self):
         self.fitness = 0
-
+        self._mutate_add_connection()
+    
+    def _mutate_add_connection(self):
+        self.genome = DefaultGenome('gadakeco')
+        self.genome.mutate_add_connection()
+        
     def update_fitness(self, points, time):
         """
         Berechnet und aktualisiert den Fitness-Wert des Netzwerks 
@@ -27,5 +35,75 @@ class Network:
                 b, ob die Taste "nach Rechts" gedrueckt ist
                 c, ob die Taste "springen" gedrueckt ist.
         """
+        if len(self.genome.input_nodes) != len(values):
+            raise RuntimeError("Expected {0:n} inputs, got {1:n}".format(len(self.genome.input_nodes), len(values)))
+        for k, v in zip(self,genome.input_nodes, values):
+            self.values[k] = v
+        for node, act_func, agg_func, bias, response, links in self.genome.node_evals:
+            node_inputs = []
+            for i, w in links:
+                node_inputs.append(self.values[i] * w)
+            s = agg_func(node_inputs)
+            self.values[node] = act_func(bias + reponse * s)
+        return [self.values[i] for i in self.genome.output_nodes]
+#         return [False, False, False]
 
-        return [False, False, False]
+class DefaultGenome(object):
+    def __init__(self, key):
+        self.key = key
+        self.connections = {}
+        self.nodes = {}
+        self.input_nodes = [f'in{i}' for i in range(486)]
+        self.hidden_nodes = []
+        self.output_nodes = ['links', 'rechts', 'springen']
+        self.node_evals = []
+        self.fitness = None
+        self.initial_connection = "full"
+        self.node_add_prob = 0.1
+        self.conn_add_prob = 0.2
+        self.single_structural_mutation = True
+        
+        # create a genome
+        self._configure()
+        
+    def _configure(self):
+        """
+         return:
+         [(node, act_func, agg_func, bias, response, links),
+         (node, act_func, agg_func, bias, response, links)]
+        """
+        # get the layers
+        layers = [self.input_nodes]
+        for _ in self.hidden_nodes:
+            layers.append(_)
+        layers.append(self.output_nodes)
+        
+        if self.initial_connection == 'full':
+            # get the connections
+            for l in range(len(layers) -1):
+                for c in itertools.product(layers[i], layers[i+1]):
+                    connections[(c[0], c[1])]['weight'] = random.uniform(0, 1)
+            
+            for layer in layers:
+                for node in layer:
+                    links = []
+                    for conn_key in connections:
+                        inode, onode = conn_key
+                        if onode == node:
+                            cg = connections[(c[0], c[1])]
+                            links.append((inode, cg.weight))
+                    self.node_evals.append(node, relu, sum, 0.0, 1.0, links)
+    
+    def mutate(self):
+        if random() < self.node_add_prob:
+            self.mutate_add_node(config)
+        if random() < self.conn_add_prob:
+            self.mutate_add_connection()
+    
+    def mutate_add_node(self):
+        conn_to_split = 
+    
+    def mutate_add_connection(self):
+        pass
+    
+            
--- Gadakeco_Code/src/neat/population.py
+++ Gadakeco_Code/src/neat/population.py
@@ -1,19 +1,27 @@
 from neat.network import Network
-
+import time
 
 class Population():
-    def __init__(self, seed, size):
+    def __init__(self, seed, size, initial_state=None):
         """
         Erstellt eine neue Population mit der Groesse 'size' und wird zuerst fuer den uebergebenen seed trainiert.
         """
         self.seed = seed
-        # Das Attribut generation_count wird von Gadakeco automatisch inkrementiert. 
-        self.generation_count = 1
+        if initial_state is None:
+            # Das Attribut generation_count wird von Gadakeco automatisch inkrementiert. 
+            self.generation_count = 1
 
-        # eindeutiger name name des Netzwerks (noch zu implementieren)
-        self.name = "name"
+            # eindeutiger name name des Netzwerks (noch zu implementieren)
+    #         self.name = "name"
+            timestr = time.strftime("%d-%m-%y_%H-%M-%S")
+            self.name = timestr
 
-        self.current_generation = [Network()]
+            self.current_generation = [Network() for _ in range(size)]
+        else:
+            self.generation_count = initial_state[0]
+            
+            #TODO reproduce the current_generation
+            config_current_generation = initial_state[1]
 
     @staticmethod
     def load_from_file(filename):
@@ -21,15 +29,27 @@ class Population():
         Laedt die komplette Population von der Datei mit dem Pfad filename.
         """
         print("called load_from_file")
-        return None
+        with gzip.open(filename) as f:
+            seed, generation_count, config_current_generation = pickle.load(f)
+        return Population(seed, 100, (generation_count, config_current_generation))
 
     def save_to_file(self, filename):
         """
         Speichert die komplette Population in die Datei mit dem Pfad filename.
         """
         print("called save_to_file")
+        # TODO get the config of current generation
+        self.config_current_generation = {(inode, onode):{"weight": , "", "", ""}, }
+        # Save the data
+        with gzip.open(filename, 'w', compresslevel=5) as f:
+#             data = (self.current_generation, config, population, species_set, random.getstate())
+            data = (self.seed, self.generation_count, self.name , self.config_current_generation)
+            pickle.dump(data, f, protocol=pickle.HIGHEST_PROTOCOL)
 
     def create_next_generation(self):
         """
         Erstellt die naechste Generation.
         """
+        # TODO genomes mutate, crossover
+        self.current_generation = []
+        
