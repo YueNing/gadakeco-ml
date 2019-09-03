9ef337d190107a7f21e081a4b176e98aaff36a20
wuxiaji <wangzheyuan78@gmail.com>
2019-09-03 17:50:24 +0800

    error by push
    我的pycharm怎么自己关掉了？？切换branch的时候误操作了？

--- Gadakeco_Code/src/neat/network.py
+++ Gadakeco_Code/src/neat/network.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import numpy as np
 import itertools
 import random
@@ -247,3 +248,39 @@ class DefaultNode(object):
 
 def signmus_activation():
     return lambda x: x and (1, -1)[x < 0]         
+=======
+
+
+class Network:
+    def __init__(self):
+        self.fitness = 0
+
+    def update_fitness(self, points, time):
+        """
+        Berechnet und aktualisiert den Fitness-Wert des Netzwerks 
+        basierend auf den Punkten (des 'Spielers') und der vergangenen Zeit.
+        """
+        self.fitness = points - 50 * time
+
+    def evaluate(self, values):
+        """
+        Wertet das Netzwerk aus. 
+        
+        Argumente:
+            values: eine Liste von 27x18 = 486 Werten, welche die aktuelle diskrete Spielsituation darstellen
+                    die Werte haben folgende Bedeutung:
+                     1 steht fuer begehbaren Block
+                    -1 steht fuer einen Gegner
+                     0 leerer Raum
+        Rueckgabewert:
+            Eine Liste [a, b, c] aus 3 Boolean, welche angeben:
+                a, ob die Taste "nach Links" gedrueckt ist
+                b, ob die Taste "nach Rechts" gedrueckt ist
+                c, ob die Taste "springen" gedrueckt ist.
+        """
+        #valuse is the input list, randomly connected to nodes
+
+            #weight_list = random.choices([-1, 0, 1], k=486)
+        #github upload test
+        return [False, False, False]
+>>>>>>> Stashed changes
